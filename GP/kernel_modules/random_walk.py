90457127f7df3c018584503755d2cb37d275fd90
Julius <julius.d.schwartz@gmail.com>
2021-10-16 12:41:47 +0200

    minor clean-up

--- GP/kernel_modules/random_walk.py
+++ GP/kernel_modules/random_walk.py
@@ -3,11 +3,10 @@
 Molecule kernels for Gaussian Process Regression implemented in GPflow.
 """
 
-import gpflow
-import numpy as np
-import tensorflow as tf
-
 from math import factorial
+
+import gpflow
+import tensorflow as tf
 
 from .kernel_utils import normalize, unpad_tensor, extract_adj_mats_from_vector_inputs
 
@@ -30,56 +29,36 @@ class RandomWalk(gpflow.kernels.Kernel):
         specifically using the spectral decomposition approach
         given by https://www.jmlr.org/papers/volume11/vishwanathan10a/vishwanathan10a.pdf
 
-        :param X: array of N graph objects (represented as adjacency matrices of varying sizes)
-        :param X2: array of M graph objects (represented as adjacency matrices of varying sizes)
-            If None, compute the N x N kernel matrix for X.
-        :return: The kernel matrix of dimension N x M.
+        :param X: N x D array.
+        :param X2: M x D array. If None, compute the N x N kernel matrix for X.
+        :return: The kernel matrix of dimension N x M
         """
 
-        X_padded = False
-        X2_padded = False
-        X_is_X2 = False
-
-        if isinstance(X, (np.ndarray, tf.Tensor)) and len(X.shape) == 2:
-            X = extract_adj_mats_from_vector_inputs(X)
-            X_padded = True
+        X = extract_adj_mats_from_vector_inputs(X)
 
         if X2 is None:
             X2 = X
-            X2_padded = X_padded
             X_is_X2 = True
-        elif isinstance(X2, (np.ndarray, tf.Tensor)) and len(X2.shape) == 2:
+        else:
             X2 = extract_adj_mats_from_vector_inputs(X2)
-            X2_padded = True
+            X_is_X2 = False
 
         flattened_k_matrix = tf.TensorArray(tf.float64, size=len(X)*len(X2))
         matrix_idx = 0
 
         for idx_1 in range(len(X)):
 
-            adj_mat_1 = X[idx_1]
-            if X_padded:
-                adj_mat_1 = unpad_tensor(adj_mat_1)
-            eigenval_1, eigenvec_1 = tf.linalg.eigh(tf.cast(adj_mat_1, tf.float64))
-            start_stop_probs = tf.ones((1, tf.shape(eigenvec_1)[0]), tf.float64)
-            if self.uniform_probabilities:
-                start_stop_probs = tf.divide(start_stop_probs, tf.shape(eigenvec_1)[0])
-            flanking_factor_1 = tf.linalg.matmul(start_stop_probs, eigenvec_1)
+            adj_mat_1 = tf.cast(unpad_tensor(X[idx_1]), tf.float64)
+            eigenval_1, eigenvec_1, flanking_factor_1 = self._eigendecompose_and_calculate_flanking_factor(adj_mat_1)
 
             for idx_2 in range(len(X2)):
 
                 if X_is_X2 and idx_1 == idx_2:
                     eigenval_2, eigenval_2, flanking_factor_2 = eigenval_1, eigenval_1, flanking_factor_1
                 else:
-                    adj_mat_2 = X2[idx_2]
-                    if X2_padded:
-                        adj_mat_2 = unpad_tensor(adj_mat_2)
-                    eigenval_2, eigenvec_2 = tf.linalg.eigh(tf.cast(adj_mat_2, tf.float64))
-                    start_stop_probs = tf.ones((1, tf.shape(eigenvec_2)[0]), tf.float64)
-                    if self.uniform_probabilities:
-                        start_stop_probs = tf.divide(start_stop_probs, tf.shape(eigenvec_2)[0])
-                    flanking_factor_2 = tf.linalg.matmul(start_stop_probs, eigenvec_2)
-
+                    adj_mat_2 = tf.cast(unpad_tensor(X2[idx_2]), tf.float64)
+                    eigenval_2, eigenvec_2, flanking_factor_2 = self._eigendecompose_and_calculate_flanking_factor(
+                        adj_mat_2)
 
                 flanking_factor = tf.linalg.LinearOperatorKronecker(
                     [tf.linalg.LinearOperatorFullMatrix(flanking_factor_1),
@@ -122,15 +101,23 @@ class RandomWalk(gpflow.kernels.Kernel):
         k_matrix = tf.reshape(flattened_k_matrix.stack(), (len(X), len(X2)))
 
         if self.normalize:
-            normalized_k_matrix = normalize(k_matrix)
-            return normalized_k_matrix
+            return normalize(k_matrix)
 
         return k_matrix
 
     def K_diag(self, X):
         """
-        Compute the diagonal of the N x N kernel matrix of X.
-        :param X: array of N graph objects (represented as adjacency matrices of varying sizes).
-        :return: N x 1 array.
+        Compute the diagonal of the N x N kernel matrix of X
+        :param X: N x D array
+        :return: N x 1 array
         """
         return tf.linalg.tensor_diag_part(self.K(X))
+
+    def _eigendecompose_and_calculate_flanking_factor(self, adjacency_matrix):
+        eigenval, eigenvec = tf.linalg.eigh(adjacency_matrix)
+        start_stop_probs = tf.ones((1, tf.shape(eigenvec)[0]), tf.float64)
+        if self.uniform_probabilities:
+            start_stop_probs = tf.divide(start_stop_probs, tf.shape(eigenvec)[0])
+        flanking_factor = tf.linalg.matmul(start_stop_probs, eigenvec)
+
+        return eigenval, eigenvec, flanking_factor
